af840640001be1f1b275bbb0fac92165bd2d38f9
Evan Martin <evanm@google.com>
2016-03-23 16:23:44 -0700

    ok

--- test_files/bracket_properties.sickle.ts
+++ test_files/bracket_properties.sickle.ts
@@ -1,6 +1,7 @@
 Error at test_files/bracket_properties.in.ts:9:1: indexing an object is unsafe in the presence of Closure renaming
 Error at test_files/bracket_properties.in.ts:22:1: indexing an object is unsafe in the presence of Closure renaming
 Error at test_files/bracket_properties.in.ts:24:1: indexing an object is unsafe in the presence of Closure renaming
+Error at test_files/bracket_properties.in.ts:30:22: IndexSignature not implemented in type literal member
 ====
 // This test is for verifying that accessing an object field
 // by string name, e.g. foo['bar'], is disallowed by sickle
@@ -23,7 +24,7 @@ class FieldsClass {
   field: string;
 
   static _sickle_typeAnnotationsHelper() {
- /** @type { string} */
+ /** @type {string} */
     FieldsClass.prototype.field;
   }
 
@@ -33,13 +34,13 @@ x3['field'];  // should error
 
 (
 /**
- * @return { Fields}
+ * @return {Fields}
  */
 (): Fields => null)()['field'];  // should error
 
-let /** number[] */ bracketTestArray: number[];
+let /** Array<number> */ bracketTestArray: number[];
 let /** Array<number> */ bracketTestArray2: Array<number>;
 bracketTestArray[1];  // ensure we didn't accidentally break arrays
 bracketTestArray2[1];
-let /** {[key: string]: number} */ bracketTestMap: {[key: string]: number};
+let /** {} */ bracketTestMap: {[key: string]: number};
 bracketTestMap['a'];  // ensure we didn't accidentally break maps
--- test_files/bracket_properties.tr.js
+++ test_files/bracket_properties.tr.js
@@ -7,7 +7,7 @@ let /** FieldsIndexable */ x2;
 x2['field']; // is ok, object is indexable
 class FieldsClass {
     static _sickle_typeAnnotationsHelper() {
-        /** @type { string} */
+        /** @type {string} */
         FieldsClass.prototype.field;
     }
 }
@@ -15,15 +15,15 @@ let /** FieldsClass */ x3;
 x3['field']; // should error
 (
 /**
- * @return { Fields}
+ * @return {Fields}
  */
 /**
- * @return { Fields}
+ * @return {Fields}
  */
     () => null)()['field']; // should error
-let /** number[] */ bracketTestArray;
+let /** Array<number> */ bracketTestArray;
 let /** Array<number> */ bracketTestArray2;
 bracketTestArray[1]; // ensure we didn't accidentally break arrays
 bracketTestArray2[1];
-let /** {[key: string]: number} */ bracketTestMap;
+let /** {} */ bracketTestMap;
 bracketTestMap['a']; // ensure we didn't accidentally break maps
